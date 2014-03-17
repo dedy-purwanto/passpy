7da5d885bd10e5dc0829f8e6c64a5f1f2eec58bb
Dedi Purwanto <kecebongsoft@gmail.com>
2014-03-17 10:24:37 +0800

    Refined with argsparse and custom salt

--- pass.py
+++ pass.py
@@ -3,6 +3,9 @@
 import os, sys
 from hashlib import sha1
 from getpass import getpass
+import argparse
+
+SUFFIX = '$#'
 
 def cap_first(pwd):
     new_pwd = []
@@ -12,40 +15,52 @@ def cap_first(pwd):
         if not s.isdigit(): caps = True
     return ''.join(new_pwd)
 
-SALT = 'abdul'
-suffix = '$#'
+def get_salt():
+    filename = '%s/.saltpass' % os.path.expanduser('~')
+    try:
+        open(filename, 'r')
+    except IOError:
+        sys.stdout.write("No default salt specified in %s, specify first.\n" % filename)
+        salt = getpass('New default salt: ')
+        f = open(filename, 'w').write(salt)
+
+    f = open(filename, 'r')
+    for s in f: return s
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-n', '--new', help='Generate-mode', action='store_true')
+parser.add_argument('-s', '--salt', help='Custom salt')
+parser.add_argument('-l', '--length', help='Set final length', type=int)
+parser.add_argument('-o', '--out', help='Output generated pass', action='store_true')
+
+args = parser.parse_args()
+
+salt = args.salt if args.salt else get_salt()
+length = args.length - 1 if args.length else 9999
 
 p = getpass()
-confirm = getpass()
+if args.new:
+    confirm = getpass()
+    if p != confirm:
+        sys.stdout.write("Password not matched!\n")
+        exit()
 
-length = 99999
+p = '%s%s' % (p, salt)
+p = sha1(p).hexdigest()
 
-if len(sys.argv) > 1:
-    try:
-        length = int(sys.argv[1])
-    except ValueError:
-        length = None
+p = p[:length - len(SUFFIX)]
 
-if p == confirm:
-    p = '%s%s' % (p, SALT)
-    p = sha1(p).hexdigest()
+p = '%s%s' % (p, SUFFIX)
 
-    p = p[:length - len(suffix)]
-    
-    p = '%s%s' % (p, suffix)
+#uppercase_length = 8
+#p = "%s%s" % (p[:uppercase_length].upper(), p[uppercase_length:])
 
+p = cap_first(p)
 
-    uppercase_length = 8
-    p = "%s%s" % (p[:uppercase_length].upper(), p[uppercase_length:])
+clip = os.popen('pbcopy', 'w')
+clip.write(p)
+clip.close()
 
-    #p = cap_first(p)
-
-    clip = os.popen('pbcopy', 'w')
-    clip.write(p)
-    clip.close()
-
-    sys.stdout.write("Show? [yes/NO] : ")
-    if raw_input().lower() == 'yes':
-        print p
-else:
-    print "Not matched, try again."
+if args.out:
+    print p
